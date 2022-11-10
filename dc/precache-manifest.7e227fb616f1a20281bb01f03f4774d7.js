3a55abfa86cb1461af0da2c7ad62d064e210b165
shuaifeng <shuaifeng121314@126.com>
2022-11-10 21:05:06 +0800

    PJ:单车项目上传

---
+++ dc/precache-manifest.7e227fb616f1a20281bb01f03f4774d7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7b5d97097df80a8269eb5bf59b606f58",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3456bce17f30489166d0",
+    "url": "./static/css/2.e328385f.chunk.css"
+  },
+  {
+    "revision": "b360dcd21b09828a6f8c",
+    "url": "./static/css/main.ec298f0e.chunk.css"
+  },
+  {
+    "revision": "3456bce17f30489166d0",
+    "url": "./static/js/2.04d61a0b.chunk.js"
+  },
+  {
+    "revision": "b303609581b33d1d1830c01690171ab3",
+    "url": "./static/js/2.04d61a0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b360dcd21b09828a6f8c",
+    "url": "./static/js/main.3dac279b.chunk.js"
+  },
+  {
+    "revision": "09bebc1dede166d43933",
+    "url": "./static/js/runtime-main.3af33c1b.js"
+  }
+]);
